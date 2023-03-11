49bfb4bbedcc60aaf87c97099c9870c3999277ae
kintrix007 <boldizsar02@gmail.com>
2023-03-11 23:42:07 +0100

    Also remove the fedora flathub filter

--- scripts/fedora-debloat-gnome.sh
+++ scripts/fedora-debloat-gnome.sh
@@ -3,10 +3,14 @@
 echo "To be implemented..."
 exit 1
 
-echo "Removing bloat packages..."
+echo
+echo "  Removing bloat packages..."
 echo
 
 sudo dnf remove gnome-contacts gnome-weather gnome-clocks gnome-maps gnome-photos totem libreoffice-* gnome-boxes gnome-tour cheese mediawriter yelp gnome-calendar -y
 
 echo
-echo "Finished"
+echo "  Removing fedora flathub filter..."
+echo
+
+flatpak remote-modify flathub --no-filter
