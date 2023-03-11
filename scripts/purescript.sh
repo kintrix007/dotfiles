b3a6458295b1805c4868b39dcb05d3f75d719fc7
kintrix007 <boldizsar02@gmail.com>
2023-03-11 22:50:58 +0100

    Added purescript install command

---
+++ scripts/purescript.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# ? https://jordanmartinez.github.io/purescript-jordans-reference-site/content/01-Getting-Started/04-Install-Guide.html
+# ? Recommended versions at the time of writing: 
+# npm i -g purescript@0.15.7 spago@0.20.9 esbuild@0.15.7 purs-tidy@0.9.2 purs-backend-es@1.3.1 purescript-psa@0.8.2
+
+npm i -g purescript spago esbuild purs-tidy purs-backend-es purescript-psa
+
+npm i -g purs-backend-es
