96c27af24c85ec18f008e304940252a3271d4b2b
kintrix007 <boldizsar02@gmail.com>
2023-03-12 00:10:47 +0100

    echo message if nix is already installed

--- scripts/fedora-basic-setup.sh
+++ scripts/fedora-basic-setup.sh
@@ -20,4 +20,6 @@ if [! -d /nix ]; then
     # ? Or alternatively, the following.
     # ? It is for multi-user installation with no SELinux.
     # sh <(curl -L https://nixos.org/nix/install) --daemon
+else
+    echo "Nothing to do. Nix already installed."
 fi
