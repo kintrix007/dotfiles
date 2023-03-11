23d5cf5d46f5f5dfc57aa4f164191438d6d056ad
kintrix007 <boldizsar02@gmail.com>
2023-03-12 00:09:22 +0100

    Add check if /nix already exists

--- scripts/fedora-basic-setup.sh
+++ scripts/fedora-basic-setup.sh
@@ -15,7 +15,9 @@ sudo dnf install -y \
   sudo dnf install -y gparted vlc mpv && \
   sudo dnf install -y papirus-icon-theme) &
 
-sh <(curl -L https://nixos.org/nix/install) --no-daemon
-# ? Or alternatively, the following.
-# ? It is for multi-user installation with no SELinux.
-# sh <(curl -L https://nixos.org/nix/install) --daemon
+if [! -d /nix ]; then
+    sh <(curl -L https://nixos.org/nix/install) --no-daemon
+    # ? Or alternatively, the following.
+    # ? It is for multi-user installation with no SELinux.
+    # sh <(curl -L https://nixos.org/nix/install) --daemon
+fi
