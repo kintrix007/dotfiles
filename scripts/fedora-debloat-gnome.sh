295046045b8dc52fd58ee057a5a9808029fdec48
kintrix007 <boldizsar02@gmail.com>
2023-03-11 23:35:54 +0100

    Removed bload packages

--- scripts/fedora-debloat-gnome.sh
+++ scripts/fedora-debloat-gnome.sh
@@ -2,3 +2,11 @@
 
 echo "To be implemented..."
 exit 1
+
+echo "Removing bloat packages..."
+echo
+
+sudo dnf remove gnome-contacts gnome-weather gnome-clocks gnome-maps gnome-photos totem libreoffice-* gnome-boxes gnome-tour cheese mediawriter yelp gnome-calendar -y
+
+echo
+echo "Finished"
