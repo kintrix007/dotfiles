8ebfcffb55e9ae2c0df64976a73de87d76b4789c
kintrix007 <boldizsar02@gmail.com>
2023-03-11 23:57:52 +0100

    Added nix install and cleaned up script

--- scripts/fedora-basic-setup.sh
+++ scripts/fedora-basic-setup.sh
@@ -1,6 +1,13 @@
 sudo dnf upgrade -y
 
 (flatpak update --noninteractive && \
-flatpak install com.mattjakeman.ExtensionManager --noninteractive) &
+  flatpak install com.mattjakeman.ExtensionManager --noninteractive) &
 
-sudo dnf install git gh vim htop neofetch papirus-icon-theme &
+(sudo dnf install -y git gh vim htop neofetch ffmpeg && \
+  sudo dnf install -y gparted vlc mpv && \
+  sudo dnf install -y papirus-icon-theme) &
+
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+# ? Or alternatively, the following.
+# ? It is for multi-user installation with no SELinux.
+# sh <(curl -L https://nixos.org/nix/install) --daemon
