ad6276d39bc51732ce3d32594ae35317ba23a403
kintrix007 <boldizsar02@gmail.com>
2023-03-12 01:43:28 +0100

    fixed missing sudo

--- scripts/installers/vs-codium-fedora.sh
+++ scripts/installers/vs-codium-fedora.sh
@@ -13,6 +13,6 @@ gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 metadata_expire=1h
-EOF >/etc/yum.repos.d/vscodium.repo
+EOF | sudo tee /etc/yum.repos.d/vscodium.repo
 
 sudo dnf install -y codium
