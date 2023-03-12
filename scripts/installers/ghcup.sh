5959b8f67fa4529f51eb0be6102a0e194c49e307
kintrix007 <boldizsar02@gmail.com>
2023-03-12 01:16:04 +0100

    Fixed ghcup installer to be non-interactive

--- scripts/installers/ghcup.sh
+++ scripts/installers/ghcup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if ! which ghcup >/dev/null 2>&1; then
-  curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+  curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | \
+    BOOTSTRAP_HASKELL_CABAL_VERSION=latest BOOTSTRAP_HASKELL_INSTALL_STACK=1 \
+    BOOTSTRAP_HASKELL_NONINTERACTIVE=1 BOOTSTRAP_HASKELL_ADJUST_BASHRC=P sh
 else
   echo "Nothing to do. GHCup already installed."
 fi
