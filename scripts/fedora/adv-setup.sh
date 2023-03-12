a0194afeea53f4fe47f2a98d82ef07c003c7f9b5
kintrix007 <boldizsar02@gmail.com>
2023-03-12 01:52:32 +0100

    Added flatpak instructions

--- scripts/fedora/adv-setup.sh
+++ scripts/fedora/adv-setup.sh
@@ -2,9 +2,16 @@
 
 sudo dnf install -y nodejs python3-pip
 
-(sudo dnf install -y vlc-bittorrent && \
+(sudo dnf remove -y firefox
+  sudo dnf install -y vlc-bittorrent
   sudo dnf install -y g++
   sudo dnf install -y steam) &
+
+flatpak install --noninteractive org.mozilla.firefox &
+flatpak install --noninteractive com.heroicgameslauncher.hgl &
+flatpak install --noninteractive org.libreoffice.LibreOffice &
+flatpak install --noninteractive org.onlyoffice.desktopeditors &
+flatpak install --noninteractive com.discordapp.Discord &
 
 sudo npm install -g npm
 
