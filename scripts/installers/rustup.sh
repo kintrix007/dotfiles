884c250e3379fdc90b4a1f792eb4f40094cf6261
kintrix007 <boldizsar02@gmail.com>
2023-03-12 01:26:21 +0100

    Made rustup installer non-interactive

--- scripts/installers/rustup.sh
+++ scripts/installers/rustup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! which rustup >/dev/null 2>&1; then
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 else
   echo "Nothing to do. Rustup already installed."
 fi
