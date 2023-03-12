1b92e302b322cb97a753948b1fdfb586d0489fbd
kintrix007 <boldizsar02@gmail.com>
2023-03-12 01:35:36 +0100

    Install more dev tools

--- scripts/fedora/adv-setup.sh
+++ scripts/fedora/adv-setup.sh
@@ -3,10 +3,13 @@
 sudo dnf install -y nodejs python3-pip
 
 (sudo dnf install -y vlc-bittorrent && \
+  sudo dnf install -y g++
   sudo dnf install -y steam) &
 
 sudo npm install -g npm
 
 ../installers/ghcup.sh
 ../installers/rustup.sh
+../installers/v.sh
+../installers/vs-codium-fedora.sh
 ../installers/itch-desktop.sh
