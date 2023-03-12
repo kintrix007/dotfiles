26c62d44240684b9a15bd45022cc6c95ccd4b26b
kintrix007 <boldizsar02@gmail.com>
2023-03-12 01:35:09 +0100

    Added V installer script

---
+++ scripts/installers/v.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if which v >/dev/null 2>&1; then
+  mkdir -p ~/.local/bin/
+  pushd ~/.local/bin/ >/dev/null
+  git clone https://github.com/vlang/v vlang
+  cd vlang
+  make
+  sudo ./v symlink
+  popd >/dev/null
+else
+  echo "Nothing to do. V already installed."
+fi
