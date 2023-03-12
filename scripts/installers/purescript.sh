712c5fcd4f16ff0e84acd0697542e04862fb89cc
kintrix007 <boldizsar02@gmail.com>
2023-03-12 01:25:24 +0100

    Added missing sudo permission

--- scripts/installers/purescript.sh
+++ scripts/installers/purescript.sh
@@ -4,6 +4,4 @@
 # ? Recommended versions at the time of writing: 
 # npm i -g purescript@0.15.7 spago@0.20.9 esbuild@0.15.7 purs-tidy@0.9.2 purs-backend-es@1.3.1 purescript-psa@0.8.2
 
-npm i -g purescript spago esbuild purs-tidy purs-backend-es purescript-psa
-
-npm i -g purs-backend-es
+sudo npm i -g purescript spago esbuild purs-tidy purs-backend-es purescript-psa purs-backend-es
