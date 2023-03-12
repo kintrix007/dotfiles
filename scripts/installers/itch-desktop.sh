6c7a062269acd416923ad36a46950db39b7ddf95
kintrix007 <boldizsar02@gmail.com>
2023-03-12 01:31:26 +0100

    Made pushd and popd output silent

--- scripts/installers/itch-desktop.sh
+++ scripts/installers/itch-desktop.sh
@@ -2,13 +2,13 @@
 
 # Figure out how to check if itch is installed
 if [ ! -f ~/.local/bin/itch-setup ]; then
-  pushd `mktemp -d`
+  pushd `mktemp -d` >/dev/null
   OUTFILE="itch-setup-linux-amd64.zip"
   wget "https://broth.itch.ovh/itch-setup/linux-amd64/1.26.0/archive/default" -O "$OUTFILE" -q
   mkdir -p ~/.local/bin/
   unzip "$OUTFILE" -d ~/.local/bin/
   ~/.local/bin/itch-setup --silent
-  popd
+  popd >/dev/null
 else
   echo "Found traces of Itch installer. Executing..."
   ~/.local/bin/itch-setup --silent
