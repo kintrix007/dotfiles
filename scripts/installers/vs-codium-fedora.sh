e0cb21c4c71f2fc3d2216b6a5119bf71fb248e30
kintrix007 <boldizsar02@gmail.com>
2023-03-12 01:25:41 +0100

    Added VSCodium install script

---
+++ scripts/installers/vs-codium-fedora.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Add GPG key
+sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+
+# Add repository
+cat <<EOF
+[gitlab.com_paulcarroty_vscodium_repo]
+name=download.vscodium.com
+baseurl=https://download.vscodium.com/rpms/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+metadata_expire=1h
+EOF >/etc/yum.repos.d/vscodium.repo
+
+sudo dnf install -y codium
