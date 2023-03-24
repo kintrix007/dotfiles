3b4370a56b208ab5356b13ff80dcb1a0d14b2091
kintrix007 <boldizsar02@gmail.com>
2023-03-24 23:57:28 +0100

    Updated nerd-fonts location to be under bin/

--- scripts/installers/nerd-fonts.sh
+++ scripts/installers/nerd-fonts.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ ! -d ~/.local/share/fonts/nerd-fonts ]; then
-  mkdir -p ~/.local/share/fonts/
-  pushd >/dev/null ~/.local/share/fonts
+if [ ! -d ~/bin/nerd-fonts ]; then
+  mkdir -p ~/bin/
+  pushd >/dev/null ~/bin/
   git clone --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts.git nerd-fonts
   popd >/dev/null
 else
-  pushd >/dev/null ~/.local/share/fonts/nerd-fonts
+  pushd >/dev/null ~/bin/nerd-fonts
   git pull
   popd >/dev/null
 fi
