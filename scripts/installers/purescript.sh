7efaa7da880c6591fb9454934112bdb650af2a9a
kintrix007 <boldizsar02@gmail.com>
2023-03-24 23:37:13 +0100

    Made purescript installer error out

--- scripts/installers/purescript.sh
+++ scripts/installers/purescript.sh
@@ -4,4 +4,6 @@
 # ? Recommended versions at the time of writing: 
 # npm i -g purescript@0.15.7 spago@0.20.9 esbuild@0.15.7 purs-tidy@0.9.2 purs-backend-es@1.3.1 purescript-psa@0.8.2
 
+echo "Still need to implement checking if it's already installed"
+exit 1
 sudo npm i -g purescript spago esbuild purs-tidy purs-backend-es purescript-psa purs-backend-es
