5342a4a17b0a1c07a100cc6d2046e0e04d24c3cd
kintrix007 <boldizsar02@gmail.com>
2023-04-03 23:32:37 +0200

    Added git fugitive config

--- config/nvim/lua/kintrix/packer.lua
+++ config/nvim/lua/kintrix/packer.lua
@@ -24,6 +24,7 @@ return require('packer').startup(function(use)
     }
 
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use('nvim-treesitter/nvim-treesitter-context', {run = ':TSUpdate'})
     use('nvim-treesitter/playground')
 
     use 'mbbill/undotree'
@@ -52,4 +53,6 @@ return require('packer').startup(function(use)
             {'L3MON4D3/LuaSnip'},     -- Required
         }
     }
+
+    use('github/copilot.vim')
 end)
