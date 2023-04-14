2f91a3bb112986acba83a35c166d1342f5a75068
kintrix007 <boldizsar02@gmail.com>
2023-04-14 16:41:03 +0200

    Added binding to make current file executable

--- config/nvim/lua/kintrix/remap.lua
+++ config/nvim/lua/kintrix/remap.lua
@@ -5,5 +5,10 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "[P]roject [V]iew" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show [E]rror" })
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "[F]ormat buffer" })
 
+-- Why the hell is this not silent?
+vim.keymap.set("n", "<leader>x", function()
+    vim.cmd("!chmod +x %")
+end, { silent = true })
+
 -- There has to be a better way than this, right?
 vim.api.nvim_set_keymap("i", "<C-y>", "copilot#Accept('<CR>')", { expr = true, silent = true })
