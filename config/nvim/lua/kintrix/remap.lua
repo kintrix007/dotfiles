649ec6eed8376235c954fe9c2494213085df23c2
kintrix007 <boldizsar02@gmail.com>
2023-04-14 16:25:35 +0200

    Added reformat keybind and reformatted file

--- config/nvim/lua/kintrix/remap.lua
+++ config/nvim/lua/kintrix/remap.lua
@@ -1,4 +1,9 @@
+-- Project navigation
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "[P]roject [V]iew" })
-vim.api.nvim_set_keymap("i", "<C-y>", "copilot#Accept('<CR>')", {expr=true, silent=true})
 
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+-- I guess it's LSP?
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show [E]rror" })
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "[F]ormat buffer" })
+
+-- There has to be a better way than this, right?
+vim.api.nvim_set_keymap("i", "<C-y>", "copilot#Accept('<CR>')", { expr = true, silent = true })
