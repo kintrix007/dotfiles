dcb13aae50dbb989819df737d1dbc254085d3dda
kintrix007 <boldizsar02@gmail.com>
2023-09-20 14:58:28 +0200

    fix: Fix VSCode extension install script

--- config/Code - OSS/.install/install.sh
+++ config/Code - OSS/.install/install.sh
@@ -1,11 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 EXTENSIONS_FILE="./extensions"
+
+POSSIBLE_CODE_NAMES=(code codium code-oss)
+
+for CODE in ${POSSIBLE_CODE_NAMES[@]}; do
+    echo "Checking for '$CODE' on PATH..."
+    if which "$CODE" >/dev/null 2>&1; then
+        break
+    fi
+done
+
+if ! which "$CODE" >/dev/null 2>&1; then
+    echo "VSCode not found on PATH." >&2
+    exit 1
+fi
+
+echo "'$CODE' found."
 
 while read ext; do
 	[[ -n "$ext" ]] || continue
 	echo "Installing '$ext'..."
-	code --install-extension "$ext" > /dev/null 2>&1
+	$CODE --install-extension "$ext" > /dev/null 2>&1
 	exit_code=$?
 	[[ $exit_code != 0 ]] && 1>&2 echo "Something went wrong installing '$ext' ($exit_code)"
 done < <(sed 's/#.*//' "$EXTENSIONS_FILE"; echo)
