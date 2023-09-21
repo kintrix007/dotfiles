28053de8dfd01dbbe9907fd22497a4af56daeb17
kintrix007 <boldizsar02@gmail.com>
2023-09-21 11:46:13 +0200

    feat: Further specify the haskell language server

--- config/nvim/after/plugin/lspconfig.lua
+++ config/nvim/after/plugin/lspconfig.lua
@@ -1,6 +1,8 @@
 local lspconfig = require("lspconfig")
 
 lspconfig.hls.setup {
+    cmd = {"haskell-language-server-wrapper", "--lsp"},
+    filetypes = { "haskell", "lhaskell" },
     settings = {
         haskell = {
             cabalFormattingProvider = "cabalfmt",
