45537761a9e6955bb1cae5830477e4ac3deefdbe
kintrix007 <boldizsar02@gmail.com>
2023-09-30 14:40:59 +0200

    feat: Migrate to blank-indent version 3

--- config/nvim/after/plugin/indent.lua
+++ config/nvim/after/plugin/indent.lua
@@ -1,8 +1,47 @@
-local indent = require("indent_blankline")
+local indent = require("ibl")
+
+function Foo()
+    local function bar()
+        local x = 10
+        local _ = x
+        local function baz()
+
+        end
+    end
+    bar()
+end
+
+local highlight = {
+    "RainbowOrange",
+    -- "RainbowViolet",
+    -- "RainbowBlue",
+    -- "RainbowGreen",
+}
+
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+end)
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
+-- vim.g.rainbow_delimiters = { highlight = highlight }
 
 indent.setup {
-    char = '┊',
-    context_char = '┊',
-    show_current_context = true,
-    show_current_context_start = false,
+    indent = {
+        char = '┊',
+        -- char = '▏',
+    },
+    scope = {
+        enabled = true,
+        highlight = highlight,
+        -- char = '┊',
+        -- char = '▏',
+        show_start = false,
+        show_end = false,
+    },
 }
