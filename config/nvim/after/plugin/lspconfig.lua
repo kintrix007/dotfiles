c10ce426b9b9bdcc966d96e0cb652c1de8d85ec7
kintrix007 <boldizsar02@gmail.com>
2023-09-30 13:58:22 +0200

    feat: Explicitly setup Language servers

--- config/nvim/after/plugin/lspconfig.lua
+++ config/nvim/after/plugin/lspconfig.lua
@@ -1,5 +1,16 @@
 local lspconfig = require("lspconfig")
 
+lspconfig.nil_ls.setup {
+    cmd = { "nil" },
+    single_file_support = true,
+}
+
+lspconfig.bashls.setup {
+    cmd = { "bash-language-server", "start" },
+    filetypes = { "sh", "bash" },
+    single_file_support = true,
+}
+
 lspconfig.hls.setup {
     cmd = {"haskell-language-server-wrapper", "--lsp"},
     filetypes = { "haskell", "lhaskell" },
@@ -7,8 +18,9 @@ lspconfig.hls.setup {
         haskell = {
             cabalFormattingProvider = "cabalfmt",
             formattingProvider = "stylish-haskell",
-        }
-    }
+        },
+    },
+    single_file_support = true,
 }
 
 lspconfig.clangd.setup {
@@ -16,8 +28,14 @@ lspconfig.clangd.setup {
     single_file_support = true,
 }
 
-lspconfig.nil_ls.setup {
-    cmd = { "nil" },
+lspconfig.ccls.setup {
+    cmd = { "ccls" },
+    single_file_support = true,
+}
+
+lspconfig.lua_ls.setup {
+    cmd = { "lua-language-server" },
+    single_file_support = true,
 }
 
 lspconfig.metals.setup {
@@ -27,6 +45,7 @@ lspconfig.metals.setup {
             configuration = false,
         },
     },
+    single_file_support = true,
 }
 
 -- lspconfig.csharp_ls.setup {}
