89287e0ea9d422d4fc42dc50a1da23e263d0fd52
kintrix007 <boldi.koller@proton.me>
2023-11-16 05:32:44 +0100

    fix: Do not add closing quotes after exclamation mark either

--- config/nvim/after/plugin/autopairs.lua
+++ config/nvim/after/plugin/autopairs.lua
@@ -6,12 +6,14 @@ autopairs.setup {
     disable_in_visalblock = true,
 }
 
+-- TODO: Do that same with single quotes too, maybe?
+
 -- print(vim.inspect(autopairs.get_rules('"')))
 local quote_rules = autopairs.get_rules('"')
 
 -- This is the part that could need maintenance when the plugin changes...
 for _, rule in pairs(quote_rules) do
-    local regex = [=[[%w%d%%%.%'%$%)%]%}]]=]
+    local regex = [=[[%w%d%%%.%!%'%$%)%]%}]]=]
     local filetypes = rule.filetypes
 
     if filetypes ~= nil and #filetypes == 1 and filetypes[1] == "vim" then
