667abefdbfae23ee79624938d1088552288ed4f1
kintrix007 <boldi.koller@proton.me>
2023-11-16 05:50:39 +0100

    feat: Do not close either single or double quotes when they follow a character

--- config/nvim/after/plugin/autopairs.lua
+++ config/nvim/after/plugin/autopairs.lua
@@ -6,20 +6,15 @@ autopairs.setup {
     disable_in_visalblock = true,
 }
 
--- TODO: Do that same with single quotes too, maybe?
-
--- print(vim.inspect(autopairs.get_rules('"')))
-local quote_rules = autopairs.get_rules('"')
-
--- This is the part that could need maintenance when the plugin changes...
-for _, rule in pairs(quote_rules) do
-    local regex = [=[[%w%d%%%.%!%'%$%)%]%}]]=]
-    local filetypes = rule.filetypes
-
-    if filetypes ~= nil and #filetypes == 1 and filetypes[1] == "vim" then
-        -- The extension has different rules for vimscript
-        rule:with_pair(cond.not_before_regex(regex))
-    else
+local function do_not_close(rules, quote)
+    -- Also add the other kind of quote not to autoclose after
+    local regex = [=[[%w%d%%%.%!%]=] .. quote .. [=[%$%)%]%}]]=]
+    for _, rule in pairs(rules) do
         rule:with_pair(cond.not_before_regex(regex))
     end
 end
+
+-- print(vim.inspect(autopairs.get_rules('"')))
+-- This is the part that could need maintenance when the plugin changes...
+do_not_close(autopairs.get_rules('"'), "'")
+do_not_close(autopairs.get_rules("'"), '"')
