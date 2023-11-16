8fb68fd7daac4693ad04e5c9d1c494f101d71e1e
kintrix007 <boldi.koller@proton.me>
2023-11-16 05:52:41 +0100

    format: Renamed the function to make more sense

--- config/nvim/after/plugin/autopairs.lua
+++ config/nvim/after/plugin/autopairs.lua
@@ -6,8 +6,8 @@ autopairs.setup {
     disable_in_visalblock = true,
 }
 
-local function do_not_close(rules, quote)
-    -- Also add the other kind of quote not to autoclose after
+local function do_not_close_quote(rules, quote)
+    -- Also add the other kind of quote not to auto-close after
     local regex = [=[[%w%d%%%.%!%]=] .. quote .. [=[%$%)%]%}]]=]
     for _, rule in pairs(rules) do
         rule:with_pair(cond.not_before_regex(regex))
@@ -16,5 +16,5 @@ end
 
 -- print(vim.inspect(autopairs.get_rules('"')))
 -- This is the part that could need maintenance when the plugin changes...
-do_not_close(autopairs.get_rules('"'), "'")
-do_not_close(autopairs.get_rules("'"), '"')
+do_not_close_quote(autopairs.get_rules('"'), "'")
+do_not_close_quote(autopairs.get_rules("'"), '"')
