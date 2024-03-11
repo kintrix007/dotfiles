700c98b7f7cac3d80a64fb63377e4667a545e769
kintrix007 <kintrix007@proton.me>
2024-03-11 01:56:24 +0100

    Add a ton of bindings for lsp actions

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -13,10 +13,15 @@ vim.api.nvim_create_autocmd("LspAttach", {
     buffermap("i", "<C-Space>", "<C-x><C-o>", { desc = "Trigger code completion" })
 
     buffermap("n", "gd", vim.lsp.buf.definition, { desc = "[G]o to [d]efinition" })
-    buffermap("n", "K", vim.lsp.buf.hover, { desc = "Show [h]over hint" })
+    buffermap("n", "gD", vim.lsp.buf.declaration, { desc = "[G]o to [d]eclaration" })
+    buffermap("n", "gi", vim.lsp.buf.implementation, { desc = "[G]o to [i]mplementation" })
+    buffermap("n", "go", vim.lsp.buf.type_definition, { desc = "[G]o to type definition" })
+    buffermap("n", "K", vim.lsp.buf.hover, { desc = "Show hover hint" })
+    buffermap("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "Show Signature help" })
     buffermap("n", "<F2>", vim.lsp.buf.rename, { desc = "Rename symbol" })
     buffermap("n", "<F3>", vim.lsp.buf.format, { desc = "Format buffer" })
     buffermap("n", "<F4>", vim.lsp.buf.code_action, { desc = "Code action" })
     buffermap("n", "<leader>ff", vim.lsp.buf.format, { desc = "[F]ormat buffer" })
+    buffermap("n", "gr", vim.lsp.buf.references, { desc = "[G]o the [r]eferences" })
   end,
 })
