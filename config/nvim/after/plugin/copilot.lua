426a5ef17d76dc45aad353f8548b9baf031696a5
kintrix007 <kintrix007@proton.me>
2024-03-30 13:30:13 +0100

    Autoformat copilot init

--- config/nvim/after/plugin/copilot.lua
+++ config/nvim/after/plugin/copilot.lua
@@ -1,47 +1,47 @@
 local copilot = require("copilot")
 
 copilot.setup {
-    panel = {
-        enabled = true,
-        auto_refresh = false,
-        keymap = {
-            jump_prev = "[[",
-            jump_next = "]]",
-            accept = "<CR>",
-            refresh = "gr",
-            open = "<M-CR>"
-        },
-        layout = {
-            position = "bottom", -- | top | left | right
-            ratio = 0.4
-        },
+  panel = {
+    enabled = true,
+    auto_refresh = false,
+    keymap = {
+      jump_prev = "[[",
+      jump_next = "]]",
+      accept = "<CR>",
+      refresh = "gr",
+      open = "<M-CR>"
     },
-    suggestion = {
-        enabled = true,
-        auto_trigger = false,
-        debounce = 75,
-        keymap = {
-            accept = "<M-l>",
-            accept_word = false,
-            accept_line = false,
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-]>",
-        },
+    layout = {
+      position = "bottom",       -- | top | left | right
+      ratio = 0.4
     },
-    filetypes = {
-        yaml = false,
-        -- markdown = false,
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        -- haskell = false,
-        -- rust = false,
-        ["."] = false,
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = false,
+    debounce = 75,
+    keymap = {
+      accept = "<M-l>",
+      accept_word = false,
+      accept_line = false,
+      next = "<M-]>",
+      prev = "<M-[>",
+      dismiss = "<C-]>",
     },
-    copilot_node_command = 'node', -- Node.js version must be > 18.x
-    server_opts_overrides = {},
+  },
+  filetypes = {
+    yaml = false,
+    -- markdown = false,
+    help = false,
+    gitcommit = false,
+    gitrebase = false,
+    hgcommit = false,
+    svn = false,
+    cvs = false,
+    -- haskell = false,
+    -- rust = false,
+    ["."] = false,
+  },
+  copilot_node_command = 'node',   -- Node.js version must be > 18.x
+  server_opts_overrides = {},
 }
