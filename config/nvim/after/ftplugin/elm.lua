b5ef3138b6d9d7271c048b6343212905597215c6
kintrix007 <kintrix007@proton.me>
2024-06-09 15:51:07 +0200

    Add config for elm -- and set indent-size in html and ts

---
+++ config/nvim/after/ftplugin/elm.lua
@@ -0,0 +1,27 @@
+local single_file_support = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+local root_files = {
+  "elm.json",
+}
+
+local paths = vim.fs.find(root_files, { upward = true, stop = vim.env.HOME })
+local root_dir = vim.fs.dirname(paths[1])
+
+if not single_file_support and root_dir == nil then
+  return
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.offsetEncoding = { 'utf-8', 'utf-16' }
+
+vim.lsp.start({
+  name = "Elm Language Server",
+  cmd = { "elm-language-server" },
+  root_dir = root_dir,
+  single_file_support = single_file_support,
+})
