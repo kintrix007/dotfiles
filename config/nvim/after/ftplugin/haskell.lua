33673767e6b9ecf8709d37b27f8a11fe814e11d7
kintrix007 <kintrix007@proton.me>
2024-06-23 18:22:44 +0200

    Add some more handy options to the Haskell-LS

--- config/nvim/after/ftplugin/haskell.lua
+++ config/nvim/after/ftplugin/haskell.lua
@@ -6,6 +6,7 @@ vim.opt.expandtab = true
 local root_files = {
   "stack.yaml",
   "package.yaml",
+  "project.cabal",
 }
 
 local paths = vim.fs.find(root_files, { upward = true, stop = vim.env.HOME })
@@ -13,14 +14,35 @@ local root_dir = vim.fs.dirname(paths[1])
 
 vim.lsp.start({
   name = "Haskell Language Server",
-  cmd = { "haskell-language-server-wrapper", "--lsp" },
+  cmd = {
+    "haskell-language-server-wrapper",
+    -- "--log-file", "/home/kin/.hls.log",
+    "--lsp",
+  },
   root_dir = root_dir,
   single_file_support = true,
 
   settings = {
     haskell = {
       cabalFormattingProvider = "cabalfmt",
-      formattingProvider = "stylish-haskell",
-    }
-  }
+      -- formattingProvider = "stylish-haskell",
+      formattingProvider = "fourmolu",
+      plugin = {
+        ["stylish-haskell"] = {
+          globalOn = true,
+        },
+        fourmolu = {
+          globalOn = true,
+          config = {
+            external = true,
+          },
+        },
+        rename = {
+          config = {
+            diff = true,
+          },
+        },
+      },
+    },
+  },
 })
