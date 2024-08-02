e5d6c3c18faf37f50d3247f51f3dbbb598a98ebe
kintrix007 <kintrix007@proton.me>
2024-08-02 11:41:11 +0200

    Set up Gleam LS

---
+++ config/nvim/after/ftplugin/gleam.lua
@@ -0,0 +1,25 @@
+local single_file_support = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+local root_files = {
+  "gleam.toml",
+  ".git",
+}
+
+local paths = vim.fs.find(root_files, { upward = true, stop = vim.env.HOME })
+local root_dir = vim.fs.dirname(paths[1])
+
+if not single_file_support and root_dir == nil then
+  return
+end
+
+vim.lsp.start({
+  name = "Gleam Language Server",
+  cmd = { "gleam", "lsp" },
+  root_dir = root_dir,
+  single_file_support = single_file_support,
+})
