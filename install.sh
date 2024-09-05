d457b689f533adcc10becca37a38aaf34424c747
kintrix007 <kintrix007@proton.me>
2024-09-05 19:55:22 +0200

    Finally fix up install script (long overdue)

--- install.sh
+++ install.sh
@@ -1,60 +1,88 @@
 #!/usr/bin/env bash
 
-FILEPATH=`readlink -f ${BASH_SOURCE:-$0}`
-SRC=`dirname $FILEPATH`
+set -e
 
-config-specific-install() {
-    echo "Running config scripts..."
-    echo
-    echo
+FILEPATH="$(readlink -f "${BASH_SOURCE:-$0}")"
+SRC="$(dirname "$FILEPATH")"
 
-	for f in $SRC/config/*; do
+function help() {
+  echo "Usage: ./install.sh [-h | --help] [-l | --link-only] [-s | --setup-only]"
+  echo ""
+  echo "Explanation of the options:"
+  echo ""
+  echo "   --help           Show this page"
+  echo "   --link-only      Link the config files but do not run their setup"
+  echo "                    scripts"
+  echo "   --setup-only     Run the setup scripts without symlinking any files"
+}
+
+function run-setup() {
+  echo "Running config scripts..."
+  echo "---"
+  echo
+
+	for f in "$SRC"/config/*; do
 		if [[ -f "$f/.install/install.sh" ]]; then
-            BASE=`basename "$f"`
+      BASE=$(basename "$f")
 			echo "Running '$BASE' setup scriptlet..."
 
-			pushd "$f/.install" >/dev/null &&\
-			./install.sh &&\
-			popd >/dev/null &&\
+			pushd "$f/.install" >/dev/null
+			./install.sh
+			popd >/dev/null
 
 			echo "$BASE: Done."
 		fi
 	done
 
-    echo
-    echo
-    echo "Finished running config scripts."
+  echo
+  echo "---"
+  echo "Finished running config scripts."
 }
 
-if [[ "$1" == "--script" || "$1" == "-s"  ]]; then
-    echo "Running only setup scripts..."
-	config-specific-install
-	exit
+function symlink-files() {
+  # Install home dotfiles
+  for f in "$SRC"/home/{*,.*}; do
+    ln -siv "$SRC/home/$f" ~/
+  done
+
+  # Install config files
+  mkdir -p ~/.config/
+  for f in "$SRC"/config/{*,.*}; do
+    ln -siv "$f" ~/.config/
+  done
+
+  # Install local bin
+  mkdir -p ~/.local/bin
+  for f in "$SRC"/bin/{*,.*}; do
+    ln -siv "$f" ~/.local/bin/
+  done
+}
+
+SHOULD_SETUP=1
+SHOULD_LINK=1
+while true; do
+  case "$1" in
+    -h | --help)
+      help
+      exit
+      ;;
+    -s | --setup-only)
+      shift
+      SHOULD_LINK=0
+      ;;
+    -l | --link-only)
+      shift
+      SHOULD_SETUP=0
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+if [[ $SHOULD_SETUP == 0 ]] && [[ $SHOULD_LINK == 0 ]]; then
+  echo "The options --link-only and --setup-only are mutually exclusive."
 fi
 
-# Install home dotfiles
-for f in `ls -A $SRC/home/`; do
-	ln -siv "$SRC/home/$f" ~/
-done
-
-# Install config files
-[[ -d ~/.config ]] || mkdir ~/.config/
-
-for f in $SRC/config/*; do
-	ln -siv "$f" ~/.config/
-done
-
-# Install local bin
-[[ -d ~/.local/bin ]] || mkdir ~/.local/bin
-
-for f in $SRC/bin/*; do
-	ln -siv "$f" ~/.local/bin/
-done
-
-if [[ "$1" == "--link-only" || "$1" == "-l"  ]]; then
-    echo "Skiped running setup scripts, only copied config files."
-	exit
-fi
-
-# Run local installation after all are symlinked
-config-specific-install
+[[ $SHOULD_LINK == 1 ]] && symlink-files
+[[ $SHOULD_SETUP == 1 ]] && run-setup
