f8524e08094b2f3540c2513cfe2e95bbd7a77e92
kintrix007 <kintrix007@proton.me>
2024-09-17 15:59:40 +0200

    feat: Correctly set up OmniSharp LSP for C#

---
+++ config/nvim/after/ftplugin/cs.lua
@@ -0,0 +1,29 @@
+local single_file_support = false
+
+local root_files = {
+  -- FIXME: Glob patterns don't work
+  "*.sln",
+  "*.csproj",
+  "omnisharp.json",
+  "function.json",
+  ".git",
+}
+
+local paths = vim.fs.find(root_files, { upward = true, stop = vim.env.HOME })
+local root_dir = vim.fs.dirname(paths[1])
+
+if not single_file_support and root_dir == nil then
+  return
+end
+
+local pid = vim.fn.getpid()
+
+vim.lsp.start({
+  name = "C# Language Server",
+  cmd = { "OmniSharp", "-lsp", "--hostPID", tostring(pid) },
+  root_dir = root_dir,
+  single_file_support = single_file_support,
+  on_attach = function()
+    print("OmniSharp attached.")
+  end
+})
