eff94bd7a1f6592c0a047001e23f3871c4ca52f8
kintrix007 <kintrix007@proton.me>
2025-11-06 16:22:09 +0100

    feat: Add more telescope keybinds to nvim

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,11 @@
 local telescope = require("telescope.builtin")
 
+-- By file name
 vim.keymap.set("n", "<leader><C-p>", telescope.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fF", telescope.find_files, { desc = "[F]ind [f]iles" })
 vim.keymap.set("n", "<C-p>", telescope.git_files, { desc = "Find files in git repo" })
+vim.keymap.set("n", "<leader>fg", telescope.git_files, { desc = "[F]ind files in [g]it repo" })
 
-vim.keymap.set("n", "<leader>fs", telescope.live_grep, { desc = "[F]ile [s]earch" })
+-- By file content
+vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "[F]ind in files: [g]rep" })
+vim.keymap.set("v", "<leader>fs", telescope.grep_string, { desc = "[F]ind in files: [s]tring" })
