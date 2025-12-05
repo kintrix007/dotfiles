4337fb6fda391a626584150c3045aedb4efc56fa
kintrix <sourcehut.ungraded024@passmail.net>
2025-12-05 05:19:26 +0100

    feat: Set java indent size to 4 spaces

--- config/nvim/after/plugin/lspconfig/jdtls.lua
+++ config/nvim/after/plugin/lspconfig/jdtls.lua
@@ -1,5 +1,20 @@
 local lspconfig = require("lspconfig")
 
 lspconfig.jdtls.setup {
-
+  settings = {
+    org = {
+      eclipse = {
+        jdt = {
+          core = {
+            formatter = {
+              tabulation = {
+                char = "space",
+                size = 4,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
 }
